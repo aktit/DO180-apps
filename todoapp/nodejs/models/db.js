408de21bebd56d06fa0719686b1d0b95a0a4850f
root <root@localhost.localdomain>
2022-07-21 14:38:07 +0630

    change model env name

--- todoapp/nodejs/models/db.js
+++ todoapp/nodejs/models/db.js
@@ -1,8 +1,8 @@
 
 module.exports.params = {
-  dbname: process.env.MYSQL_ENV_MYSQL_DATABASE,
-  username: process.env.MYSQL_ENV_MYSQL_USER,
-  password: process.env.MYSQL_ENV_MYSQL_PASSWORD,
+  dbname: process.env.MYSQL_DATABASE,
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
   params: {
       host: "mysql",
       port: "3306",
